826ebc1ce783f59c243c034aa36b0d9986737efb
Maksym-Maleiev <maximmaleev93@gmail.com>
2021-12-08 20:11:38 +0200

    add js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const paletteContainer = document.querySelector('.gallery');
+const cardsMarkup = createImageCardMark(galleryItems);
+
+paletteContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+paletteContainer.addEventListener('click', onPaletteContainerClick);
+
+console.log(createImageCardMark(galleryItems));
+
+function createImageCardMark(galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+        return `<div class="gallery__item">
+<a class="gallery__link" href="${original}">
+    <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+    />
+</a>
+</div>`
+    })
+        .join('');
+};
+
+function onPaletteContainerClick(evt) {
+    if (evt.target.classList.contains('.gallery')) {
+        return
+    }
+    // const onPaletteContainerClick = event.target.dataset.image;
+    // output.textContent = `Url: ${onPaletteContainerClick}`;
+    // output.style.color = onPaletteContainerClick;
+
+    console.log(evt.target);
+};
+
+
+
+
+
+// console.log(galleryItems);
