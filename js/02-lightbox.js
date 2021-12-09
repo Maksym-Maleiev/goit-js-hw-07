acc257cf017ec2947b188ab6982be87fc2312a4c
Maksym-Maleiev <maximmaleev93@gmail.com>
2021-12-09 19:20:32 +0200

    add lightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryRef = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems
+.map(({preview, original, description}) => `<li>
+<a class="gallery__item" href="${original}">
+<img
+class="gallery__image"
+src="${preview}"
+alt="${description}"
+/>
+</a></li>`)
+.join('');
+
+galleryRef.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+let gallery = new SimpleLightbox('.gallery a', { 
+        captionsData: 'alt',
+        captionPosition: 'bottom',
+        captionDelay: 250,
+    });
+
 console.log(galleryItems);
